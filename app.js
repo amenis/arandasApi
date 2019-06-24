8b3f6585502912f52b6c1ea0cdba796159fb94a9
Alvaro Fabian <amenis10_10@hotmail.com>
2019-06-23 22:17:54 -0500

    load the webservice file

--- app.js
+++ app.js
@@ -4,23 +4,27 @@
 var winston = require('winston');
 var nconf = require('nconf');
 var Chance = require('chance');
-var express = require('express');
 var path = require('path');
 var fs = require('fs');
-var app = express();
-var port = process.env.PORT || 3000;
+var ws = require('./src/webservice');
 
+
+//Node Enviroment status
 global.env = process.env.NODE_ENV || 'production';
+
+//Call the method Chance
 var chance = new Chance();
+
 //create log levels
 winston.remove(winston.transports.Console);
 
+//Create the logger
 var sysLogger = winston.createLogger({
     levels: winston.config.cli.levels,
     format: winston.format.timestamp(),
     transports: [        
         new winston.transports.Console(),
-        new winston.transports.File({filename: '/logs/error.log', level:'error'})
+        new winston.transports.File({filename: './logs/error.log', level:'error'})
     ]
 });
 
@@ -32,28 +36,7 @@ winston.info('|  |  |  |  |         |        |             |          |  |      
 winston.info('| |    | |  |         |        |             |          |  |        | |');
 winston.info('||      ||  ||||||||  |||||||  ||||||||||||  ||||||||||||  |        | ||||||||||');
 
-var configFile = path.join(__dirname, '/config.json');
-var configExist;
 
-nconf.defaults({
-    base_dir: __dirname,
-    tokens: {
-        secret: chance.hash() +chance.md5(),
-        expires: 900
-    }
-});
-
-if( nconf.get('config') ) {
-    configFile = path.resolve(__dirname, nconf.get('config'));
-} 
-
-configExist = fs.existsSync(configFile);
-
-
-
-app.listen(port, () => {
-    console.log('THE SERVER IS RUNNING ON PORT ', port);
-    //console.log(nconf.get('tokens'));
-   
-});
-
+ws.listen( ()=> {
+    winston.info('webService is Ready');
+} );
