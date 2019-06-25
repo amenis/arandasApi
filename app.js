9aa8eb53fc0d95d71660c202195075275aff6c38
Fabian <Fabian>
2019-06-25 17:03:34 -0500

    start

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 var logger = require('./logger');
 var nconf = require('nconf');
 var Chance = require('chance');
@@ -8,40 +7,42 @@ var path = require('path');
 var fs = require('fs');
 var ws = require('./src/webservice');
 var db = require('./src/database');
-nconf.argv()
-.env()
-.file({ file: './config.json' });
+
+//nconf.argv().env().file({file: 'config.json'});
+
+var configFile = path.join(__dirname,'/config.json');
+if( nconf.get('config') ) {
+    configFile = path.resolve(__dirname, nconf.get('config'));
+}
+
 
 //Call the method Chance
 var chance = new Chance();
 
-nconf.defaults({
-    base_dir: __dirname,
-    tokens: {
-        secret: chance.hash() + chance.md5(),
-        expired: 900
-    }
-});
+console.log('°        °  °°°°°°°°  °        °°°°°°°°°°°°  °°°°°°°°°°°°  °°      °° °°°°°°°°°°');
+console.log('°        °  °         °        °             °          °  ° °    ° ° °');
+console.log('°        °  °         °        °             °          °  °  °  °  ° °');
+console.log('°   °°   °  °°°°°°    °        °             °          °  °   °°   ° °°°°°°°');
+console.log('°  °  °  °  °         °        °             °          °  °        ° °');
+console.log('° °    ° °  °         °        °             °          °  °        ° °');
+console.log('°°      °°  °°°°°°°°  °°°°°°°  °°°°°°°°°°°°  °°°°°°°°°°°°  °        ° °°°°°°°°°°');
 
 
-console.log('|        |  ||||||||  |        ||||||||||||  ||||||||||||  ||      || ||||||||||');
-console.log('|        |  |         |        |             |          |  | |    | | |');
-console.log('|        |  |         |        |             |          |  |  |  |  | |');
-console.log('|   ||   |  ||||||    |        |             |          |  |   ||   | |||||||');
-console.log('|  |  |  |  |         |        |             |          |  |        | |');
-console.log('| |    | |  |         |        |             |          |  |        | |');
-console.log('||      ||  ||||||||  |||||||  ||||||||||||  ||||||||||||  |        | ||||||||||');
+  
 
 //start the server
-ws.listen( function() { 
-    if(err) logger.error(err);
-    else logger.info(db);
-});
 
-db.initDB( (err , db) => {
-    if(err) {
-        logger.warn('Cant\'t to connect to MongoDB '+ err);
-    } else {
-       
-    }
-})
+function start() {  
+
+    db.initDB( (err , db) => {
+        if(err) {
+            logger.warn('Cant\'t to connect to MongoDB '+ err);
+        } else {
+            ws.listen( function() { 
+               logger.info('server initiated');
+            });
+        }
+    })
+}
+
+start();
