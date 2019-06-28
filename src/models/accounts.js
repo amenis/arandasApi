464603942177cc971691c9f88cea830b5b3bb566
= <amenis10_10@hotmail.com>
2019-06-28 17:12:23 -0500

    register date

--- src/models/accounts.js
+++ src/models/accounts.js
@@ -2,13 +2,14 @@
 
 var validator = require('validator');
 var mongoose = require('mongoose');
+var moment = require('moment');
 var Schema = mongoose.Schema;
 
 var userSchema = Schema({
     name: { type: String },
     surname: { type: String },
     birthdate: { type: Date },
-    registerDate: { type: Date },
+    registerDate: { type: String, default: moment().format('MM/DD/YYYY,  h:mm:ss ') },
     email: {
         type: String,
         required: true,
