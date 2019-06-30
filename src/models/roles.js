990ec484fa35e989c3ee616d6d27d3b581897ad9
Alvaro Fabian <amenis10_10@hotmail.com>
2019-06-30 13:50:19 -0500

    create roles

--- src/models/roles.js
+++ src/models/roles.js
@@ -2,81 +2,18 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var async = require('async');
-
-roles.admin = { 
-    name: "Admin", 
-    description: "", 
-    resource : [ 
-     { 
-      id : '', 
-      permissions: ['create', 'read', 'update', 'delete'] 
-     }, 
-     { 
-      id : '', 
-      permissions: ['create', 'read', 'update', 'delete'] 
-     }, 
-     { 
-      id : '', 
-      permissions: ['create', 'read', 'update', 'delete'] 
-     }, 
-
-    ] 
-}; 
-
-roles.seller = { 
-  
-    name: "seller", 
-    description: "", 
-    resource : [ 
-     { 
-      id : '', 
-      permissions: ['create', 'read', 'update', 'delete'] 
-     }, 
-     { 
-      id : '', 
-      permissions: ['read'] 
-     }, 
-     { 
-      id : '', 
-      permissions: ['create', 'read', 'update'] 
-     }, 
-
-    ] 
-};
-
-roles.user = {
-   
-    name: "user", 
-    description: "", 
-    resource : [ 
-     { 
-      id : '', 
-      permissions: ['create', 'read', 'update', 'delete'] 
-     }, 
-     { 
-      id : '', 
-      permissions: ['read'] 
-     }, 
-     { 
-      id : '', 
-      permissions: ['create', 'read', 'update'] 
-     }, 
-
-    ] 
-}
 
 var roleSchema = new Schema({
     name: { type: String },
     description: { type: String },
-    resource: [{ type: String }],
-
+    grants: [{ type: String }],
+    normalize: { type: String }
 });
 
+roleSchema.statics.getRoleByName = function (name, callback) {
+    var q = this.model('roles').findOne({ normalized: new RegExp('^' + name.trim() + '$', 'i') })
+    return q.exec(callback)
+};
 
-async.parallel([
-    
-    function done(done) {
-        roleSchema.create();
-    }
-]);
+
+module.exports = mongoose.model('roles', roleSchema);
