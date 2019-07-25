e56ccbfe1e2c74ca2fb4656d4dc0c719de0a960e
= <amenis10_10@hotmail.com>
2019-07-25 16:46:16 -0500

    createOrder

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -3,35 +3,45 @@
 var logger = require('../../logger');
 var _ = require('lodash');
 var validator = require('validator');
-var kardex = require('../models/kardex');
+var kardexModel = require('../models/kardex');
 
 var orders = {};
 
 orders.createOrder = (req, res) => {
     //we get the data from the user request
     var postData = req.body;   
-
-    if( !_.isObject(postData) || !_.isNull(req.body._id) ) {
-        res.status(400).send({ message: 'Datos invalidos' });
-    } 
-
+    var kardex = new kardexModel(postData);
+           
+    //validate data
     var validatorStore = !validator.isEmpty(postData.store);
-    var validatorProduct = !validator.isEmpty(postData.product);
-    var validatorQuantity = !validator.isEmpty(postData.quantity) ? validator.isInt(postData.quantity) : false; 
+    //var validatorProduct = !validator.isEmpty(postData.product);
+    //var validatorQuantity = !validator.isEmpty(postData.quantity) ? validator.isInt(postData.quantity) : false; 
     
     
-    if(validatorStore && validatorProduct && validatorQuantity) {
+    if(validatorStore) {
         
-
+        kardex.userAccount = postData.user;
+        kardex.comment = postData.comments;
+        kardex.status = 'order:open';
+        kardex.shoppingCart = postData.shoppingCart  
+        
+        kardex.save( (err, orderSaved) => {
+            if(err) {
+                logger.warn(err);
+                res.status(500).send({message: '!Ah ocurrido un error en el servidor'})
+            } else {
+                if(!orderSaved) {
+                    res.status(400).send({message: 'No ha sido posible guardar los datos'});
+                } else {
+                    res.status(200).send(orderSaved);
+                }
+            }
+        } );
+        
     } else {
         res.status(404).send({ message: 'Los datos no se registraron correctamente' });
     }
 
 }
 
-
-
-
-moduele.export = () => {
-    orders
-}
+module.exports = orders;
