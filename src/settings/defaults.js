d9437cbe883dc593bbe03a7de58981457997eb2f
= <amenis10_10@hotmail.com>
2019-07-29 13:39:26 -0500

    check if exist defualts

--- src/settings/defaults.js
+++ src/settings/defaults.js
@@ -51,20 +51,20 @@ var createRolesByDefaut = (callback)=> {
             //create role admin            
             rolesSchema.getRoleByName('admin',(err, role)=> {
                 if(err) logger.warn( done(err) )
-                if(role) logger.info(done())
-
-                rolesSchema.create({
-                    name: rolesDefault.admin.name,
-                    description: rolesDefault.admin.description,
-                    grants: rolesDefault.admin.grants,
-                    normalize: 'admin'
-                }, (err, roleCreated)=> {
-                    if(err) {
-                        logger.error(err);
-                    } 
-                     
-                    done()                   
-                });
+                if(!role) {
+                    rolesSchema.create({
+                        name: rolesDefault.admin.name,
+                        description: rolesDefault.admin.description,
+                        grants: rolesDefault.admin.grants,
+                        normalize: 'admin'
+                    }, (err, roleCreated)=> {
+                        if(err) {
+                            logger.error(err);
+                        } 
+                         
+                        done()                   
+                    });
+                }              
             });
             
         },
@@ -72,20 +72,20 @@ var createRolesByDefaut = (callback)=> {
             //create role seller            
             rolesSchema.getRoleByName('seller',(err, role)=> {
                 if(err) logger.warn( done(err) )
-                if(role) logger.info(done())
-
-                rolesSchema.create({
-                    name: rolesDefault.sellers.name,
-                    description: rolesDefault.sellers.description,
-                    grants: rolesDefault.sellers.grants,
-                    normalize: 'sellers'
-                }, (err, roleCreated)=> {
-                    if(err) {
-                        logger.error(err);
-                    } 
-                    
-                    done()
-                });
+                if(!role) {
+                    rolesSchema.create({
+                        name: rolesDefault.sellers.name,
+                        description: rolesDefault.sellers.description,
+                        grants: rolesDefault.sellers.grants,
+                        normalize: 'sellers'
+                    }, (err, roleCreated)=> {
+                        if(err) {
+                            logger.error(err);
+                        } 
+                        
+                        done()
+                    });
+                }              
             });
          
         },
@@ -93,40 +93,44 @@ var createRolesByDefaut = (callback)=> {
             //create role deliveryMan            
             rolesSchema.getRoleByName('deliveryMan',(err, role)=> {
                 if(err) logger.warn( done(err) )
-                if(role) logger.info(done())
+                if(!role) {
+                    rolesSchema.create({
+                        name: rolesDefault.deliveryMan.name,
+                        description: rolesDefault.deliveryMan.description,
+                        grants: rolesDefault.deliveryMan.grants,
+                        normalize: 'deliveryMan'
+                    }, (err, roleCreated)=> {
+                        if(err) {
+                            logger.error(err);
+                        } 
+                       
+                        done()
+                    });
+                }
 
-                rolesSchema.create({
-                    name: rolesDefault.deliveryMan.name,
-                    description: rolesDefault.deliveryMan.description,
-                    grants: rolesDefault.deliveryMan.grants,
-                    normalize: 'deliveryMan'
-                }, (err, roleCreated)=> {
-                    if(err) {
-                        logger.error(err);
-                    } 
-                   
-                    done()
-                });
+                
             });
         },
         function(done){
             //create role user            
             rolesSchema.getRoleByName('user',(err, role)=> {
                 if(err) logger.warn( done(err) )
-                if(role) logger.info(done())
+                if(!role) {
+                    rolesSchema.create({
+                        name: rolesDefault.user.name,
+                        description: rolesDefault.user.description,
+                        grants: rolesDefault.user.grants,
+                        normalize: 'user'
+                    }, (err, roleCreated)=> {
+                        if(err) {
+                            logger.error(err);
+                        } 
+                        
+                        done()
+                    });
+                }
 
-                rolesSchema.create({
-                    name: rolesDefault.user.name,
-                    description: rolesDefault.user.description,
-                    grants: rolesDefault.user.grants,
-                    normalize: 'user'
-                }, (err, roleCreated)=> {
-                    if(err) {
-                        logger.error(err);
-                    } 
-                    
-                    done()
-                });
+               
             });
         }
     ], (err)=> {
